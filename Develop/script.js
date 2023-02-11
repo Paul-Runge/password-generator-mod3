2b4c285b180f04e5c749c0b053016c3f140ff186
Paul-Runge <brfirestarter@gmail.com>
2023-02-11 13:27:13 -0800

    started password generator function; added prompts to confirm character types and length

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,23 @@ var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerAlpha = "abcdefghijklmnopqrstuvwxyz";
 var specChar = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-// Receptacle to hold chosen character types
+// Receptacle to hold chosen character types later on
 var chosenChar = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Generate a random password
+var generatePassword = function() {
+  // Confirm character types
+  var confirmNum = confirm("Include numbers? (click OK to confirm)");
+  var confirmUpper = confirm("Include uppercase letter? (click OK to confirm)");
+  var confirmLower = confirm("Include lowercase letters? (click OK to confirm)");
+  var confirmSpec = confirm("Include special characters? (click OK to confirm)");
+
+  //Confirm password character length
+  var length = prompt("Enter password length between 8 and 128 characters. (click OK to confirm)")
+}
 
 // Write password to the #password input
 function writePassword() {
