2b2cb187a500c2bc35c1644eac391d93b6088a09
Paul-Runge <brfirestarter@gmail.com>
2023-02-12 11:54:02 -0800

    added code blocks to validate user selections for character type and length meet requirements

--- Develop/script.js
+++ Develop/script.js
@@ -12,14 +12,30 @@ var generateBtn = document.querySelector("#generate");
 
 // Generate a random password
 var generatePassword = function() {
+  //Confirm password character length
+  var confirmLength = prompt("Enter password length between 8 and 128 characters. (click OK to confirm)");
+
+ 
   // Confirm character types
   var confirmNum = confirm("Include numbers? (click OK to confirm)");
   var confirmUpper = confirm("Include uppercase letter? (click OK to confirm)");
   var confirmLower = confirm("Include lowercase letters? (click OK to confirm)");
   var confirmSpec = confirm("Include special characters? (click OK to confirm)");
 
-  //Confirm password character length
-  var length = prompt("Enter password length between 8 and 128 characters. (click OK to confirm)")
+  // Validate character length meets requirements
+  if (confirmLength < 8 || confirmLength > 128) {
+    alert("Your password is not between 8 and 128 characters.")
+    var confirmLength = prompt("Enter password length between 8 and 128 characters. (click OK to confirm)"); 
+  }    
+  
+  // Validate atleast one character type chosen
+  if (!confirmNum && !confirmUpper && !confirmLower && !confirmSpec) {
+    alert("Please choose atlest one character type.")
+    var confirmNum = confirm("Include numbers? (click OK to confirm)");
+    var confirmUpper = confirm("Include uppercase letter? (click OK to confirm)");
+    var confirmLower = confirm("Include lowercase letters? (click OK to confirm)");
+    var confirmSpec = confirm("Include special characters? (click OK to confirm)");
+  }
 }
 
 // Write password to the #password input
